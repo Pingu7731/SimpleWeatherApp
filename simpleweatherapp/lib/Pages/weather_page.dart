22d6d28abb3cb2846239c9821121d52bdd006a3e
NootNoot <101384541+AltonChai@users.noreply.github.com>
2024-07-07 17:52:10 +0800

    Added pages
    weatherpage added

---
+++ simpleweatherapp/lib/Pages/weather_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 237, 164, 250),
+      body: Center(),
+    );
+  }
+}
--- simpleweatherapp/lib/main.dart
+++ simpleweatherapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simpleweatherapp/Pages/weather_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: WeatherPage(),
     );
   }
 }
