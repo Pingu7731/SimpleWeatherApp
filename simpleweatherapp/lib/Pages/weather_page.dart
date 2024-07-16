ce421c85e7dfdf62105fb1c7edbd2b39bf9e9387
NootNoot <101384541+AltonChai@users.noreply.github.com>
2024-07-16 19:26:39 +0800

    Fixed animation not changing
    fixed animation not changeing from weather
    
    and also did some padding

--- simpleweatherapp/lib/Pages/weather_page.dart
+++ simpleweatherapp/lib/Pages/weather_page.dart
@@ -37,7 +37,7 @@ class _WeatherPageState extends State<WeatherPage> {
   String getWeatherAnimation(String? mainCondition) {
     //default null anim
     if (mainCondition == null) return 'assets/error.json';
-    switch (mainCondition) {
+    switch (mainCondition.toLowerCase()) {
       case 'clouds':
         return 'assets/cloudy.json';
       case 'mist':
@@ -81,34 +81,41 @@ class _WeatherPageState extends State<WeatherPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             //city name
-            Text(
-              _weather?.city ?? "Lemme see where da fuq are you",
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                _weather?.city ?? "Lemme see where da fuq are you",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
               ),
             ),
 
             //animation
             Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
             //temp
-            Text(
-              _weather?.mainCondition ?? "Unlucky",
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                _weather?.mainCondition ?? "Unlucky",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 250),
+              padding: const EdgeInsets.only(
+                  top: 80, bottom: 50, left: 20, right: 20),
               child: Text(
                 '${_weather?.temperature.round()}°C',
                 style: const TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 50,
+                  fontSize: 100,
                   color: Colors.white,
                 ),
               ),
