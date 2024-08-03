9e3f57cfc5f9e6fab663e18af269bc42d6a84521
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-08-04 05:11:36 +0800

    RefreshIndicator
    - Added Pull to refresh

--- simpleweatherapp/lib/Pages/weather_page.dart
+++ simpleweatherapp/lib/Pages/weather_page.dart
@@ -76,48 +76,57 @@ class _WeatherPageState extends State<WeatherPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 21, 21, 21),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: RefreshIndicator(
+        onRefresh: () {
+          return fetchWeather();
+        },
+        child: ListView(
           children: [
-            //city name
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                _weather?.city ?? "Lemme see where da fuq are you",
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-              ),
-            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //city name
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      _weather?.city ?? "Lemme see where da fuq are you",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
 
-            //animation
-            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
-            //temp
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                _weather?.mainCondition ?? "Unlucky",
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 80, bottom: 50, left: 20, right: 20),
-              child: Text(
-                '${_weather?.temperature.round()}°C',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 100,
-                  color: Colors.white,
-                ),
+                  //animation
+                  Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+                  //temp
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _weather?.mainCondition ?? "Unlucky",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 80, bottom: 50, left: 20, right: 20),
+                    child: Text(
+                      '${_weather?.temperature.round()}°C',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
